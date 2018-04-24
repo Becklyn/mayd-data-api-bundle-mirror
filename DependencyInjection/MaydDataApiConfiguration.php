8ce40ace2c7df09730c01763e291f2fda53affc7
Jannik Zschiesche <jz@becklyn.com>
2018-04-24 11:36:13 +0200

    First draft

---
+++ DependencyInjection/MaydDataApiConfiguration.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Mayd\DataApiBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+
+class MaydDataApiConfiguration implements ConfigurationInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function getConfigTreeBuilder ()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root("mayd_data_api");
+
+        $rootNode
+            ->children()
+                ->scalarNode("project")
+                ->end()
+                ->scalarNode("secret")
+                ->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+
+}
